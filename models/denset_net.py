f1ff176d30bdbb01931dc8440149cf335122f887
fighting! <37240099+lingtengqiu@users.noreply.github.com>
2021-04-27 23:57:16 +0800

    Update denset_net.py

--- models/denset_net.py
+++ models/denset_net.py
@@ -3,7 +3,7 @@
 
 # **********************************************************
 # * Author        : lingteng qiu
-# * Email         : 1259738366@qq.com
+# * Email         : 
 # * Create time   : 2018-08-11 19:07
 # * Last modified : 2018-08-11 19:07
 # * Filename      : model.py
