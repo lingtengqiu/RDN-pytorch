6fa763cd5d9e1d3c4e648a9f38adc7dca663229e
fighting! <37240099+lingtengqiu@users.noreply.github.com>
2021-04-27 23:54:37 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -3,7 +3,7 @@
 
 # **********************************************************
 # * Author        : lingteng qiu
-# * Email         : 1259738366@qq.com
+# * Email         : 
 # * Create time   : 2018-08-16 10:28
 # * Last modified : 2018-08-16 10:28
 # * Filename      : __init__.py
