86feb5f8d875a16da0b2e261ca390368f42e16fe
fighting! <37240099+lingtengqiu@users.noreply.github.com>
2021-04-27 23:57:44 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -3,7 +3,7 @@
 
 # **********************************************************
 # * Author        : lingteng qiu
-# * Email         : 1259738366@qq.com
+# * Email         : 
 # * Create time   : 2018-08-16 10:33
 # * Last modified : 2018-08-16 10:33
 # * Filename      : dataset.py
